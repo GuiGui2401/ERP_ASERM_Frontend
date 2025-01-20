e82bed53e4a79ae4a64117632bbe0aa2accb56f3
GuiGui2401 <jguiyo@gmail.com>
2025-01-20 17:58:01 +0100

    update some files

--- src/service-worker.js
+++ src/service-worker.js
@@ -63,7 +63,6 @@ registerRoute(
 );
 
 // This allows the web app to trigger skipWaiting via
- registration.waiting.postMessage({type: 'SKIP_WAITING'})
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
